3b3acea65328da6c1bec3e228294f91521b25b7b
Sondro <memoiran@hotmail.com>
2020-09-14 17:17:23 -0700

    Add files via upload

--- CPP_WASM_-_3D_Engine/Water.h
+++ CPP_WASM_-_3D_Engine/Water.h
@@ -2,7 +2,7 @@
 
 #include <Kore/Math/Matrix.h>
 
-extern inline void water_update(Kore::mat4 matrix, Kore::mat4 vmatrix, Kore::vec3 cam, float zposition);
-extern inline void water_render(Kore::mat4 matrix, Kore::mat4 vmatrix, Kore::vec3 camera, float zposition);
+extern inline void water_update(Kore::mat4 matrix_, Kore::mat4 viewMatrix_, Kore::vec3 post_, float z_);
+extern inline void water_render(Kore::mat4 matrix_, Kore::mat4 viewMatrix_, Kore::vec3 post_, float z_);
 
 extern inline void water_init();
