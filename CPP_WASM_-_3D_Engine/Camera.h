35fc6cc3d5b88228d213bd5559fcd51e906470a7
Sondro <memoiran@hotmail.com>
2020-09-14 17:16:41 -0700

    Add files via upload

--- CPP_WASM_-_3D_Engine/Camera.h
+++ CPP_WASM_-_3D_Engine/Camera.h
@@ -12,13 +12,13 @@
 struct Camera {
 	struct Time* time;
 
-	float camNearPlane = 0.01f;
-	float camFarPlane = 1000;
+	float nearPlane = 0.01f;
+	float farPlane = 1000;
 
-	float camRotateSpeed = 0.05f;
-	float camSpeed = 5.0f;
+	float rotateSpeed = 0.05f;
+	float speed = 5.0f;
 
-	float camSpeed_divBy2 = camSpeed / 2.0f;
+	float speed_divBy2 = speed / 2.0f;
 
 	float ceiling = 2.0f;
 	const float offsetHeight = 1.0f;
@@ -26,17 +26,17 @@ struct Camera {
 	bool rotate = false;
 	bool mouseLook = true;
 
-	Kore::vec4 camUp = Kore::vec4(0.0f, 1.0f, 0.0f, 0.0f);
-	Kore::vec4 camForward = Kore::vec4(0.0f, 0.0f, 1.0f, 0.0f);
-	Kore::vec4 camRight = Kore::vec4(1.0f, 0.0f, 0.0f, 0.0f);
+	Kore::vec4 up = Kore::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+	Kore::vec4 forward = Kore::vec4(0.0f, 0.0f, 1.0f, 0.0f);
+	Kore::vec4 right = Kore::vec4(1.0f, 0.0f, 0.0f, 0.0f);
 
-	//Kore::vec3 cameraPos = Kore::vec3(0, 0, 0);
-	Kore::vec3 cameraPos = Kore::vec3(-1, 6, -5);
-	Kore::vec3 lastPos = cameraPos;
+	//Kore::vec3 post = Kore::vec3(0, 0, 0);
+	Kore::vec3 post = Kore::vec3(-1, 6, -5);
+	Kore::vec3 oldPost = post;
 	
-	int FoV = 120; //45, 60, 90
-	Kore::mat4 projection;// = Kore::mat4::Perspective(FoV, sys->screenDivByWnH, camNearPlane, camFarPlane);
-	//Kore::mat4 projectionInvert = camera_getViewMatrix(camera_).Invert();
+	int FoV = 120; //45; 60; 90;
+	Kore::mat4 projection; // = Kore::mat4::Perspective(FoV, sys->screenDivByWnH, nearPlane, farPlane);
+
 	Kore::mat4 viewMatrix;
 	Kore::vec3 _0p0f_1p0f_0p0f = Kore::vec3(0.0f, 1.0f, 0.0f);
 	Kore::vec3 viewMatrix_vec3 = _0p0f_1p0f_0p0f;
@@ -45,23 +45,23 @@ struct Camera {
 	float yClip;
 
 	Kore::mat4 inverseProView;
-	Kore::vec4 positionClip;
-	Kore::vec4 positionWorld;
+	Kore::vec4 postClip;
+	Kore::vec4 postWorld;
 
 	Kore::Quaternion quat00;
 	Kore::Quaternion quat01;
 	Kore::mat4 mat;
 	                       
-	float horizontalAngle = -3.895574891f; //-1.24f * sys->pi; (3.141592654f)
+	float horizontalAngle = -3.895574891f; // = -1.24f * sys->pi; //3.141592654f
 
 	float verticalAngle = -0.5f;
 
 };
 
-inline Kore::vec3 camera_screenToWorldSpace(struct Camera* camera_, float posX, float posY);
+inline Kore::vec3 camera_screenToWorldSpace(struct Camera* camera_, float x_, float y_);
 extern inline Kore::mat4 camera_getProjectionMatrix(struct Camera* camera_);
 extern inline Kore::mat4 camera_getViewMatrix(struct Camera* camera_);
 
-extern inline void camera_update(struct Camera* camera_, struct Input* input_, float limit, float deltaT);
-extern inline void camera_mouseLook(struct Sys* sys_, float movementX, float movementY);
+extern inline void camera_update(struct Camera* camera_, struct Input* input_, float limit_, float timeShift_);
+extern inline void camera_mouseLook(struct Sys* sys_, float goX_, float goY_);
 extern inline struct Camera* camera_init(struct Camera* camera_);
