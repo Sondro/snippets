44d03a5411cf0395aa5f953e4864bfc3f0ec791d
Sondro <memoiran@hotmail.com>
2020-09-14 17:17:05 -0700

    Add files via upload

--- CPP_WASM_-_3D_Engine/Water.cpp
+++ CPP_WASM_-_3D_Engine/Water.cpp
@@ -97,7 +97,8 @@ inline void water_init() {
 	indexBuffer->unlock();
 }
 
-inline void water_update(Kore::mat4 matrix, Kore::mat4 vmatrix, Kore::vec3 cam, float zposition) {
+inline void water_update(Kore::mat4 matrix_, Kore::mat4 viewMatrix_, Kore::vec3 camera_, float z_) {
+
 	t = sys_getTime(sys) - timeOffset;
 		/*
 		if (t > 60 * 10) {
@@ -111,14 +112,14 @@ inline void water_update(Kore::mat4 matrix, Kore::mat4 vmatrix, Kore::vec3 cam, 
 	}
 }
 
-inline void water_render(Kore::mat4 matrix, Kore::mat4 vmatrix, Kore::vec3 cam, float zposition) {
+inline void water_render(Kore::mat4 matrix_, Kore::mat4 viewMatrix_, Kore::vec3 post_, float z_) {
 	Kore::Graphics4::setPipeline(pipeline);
 	Kore::Graphics4::setFloat(timeLocation, (float)t );
-	Kore::Graphics4::setFloat(zoffsetLocation, zposition - 5.0f);
-	Kore::Graphics4::setMatrix(matrixLocation, matrix);
-	Kore::Graphics4::setMatrix(vmatrixLocation, vmatrix);
+	Kore::Graphics4::setFloat(zoffsetLocation, z_ - 5.0f);
+	Kore::Graphics4::setMatrix(matrixLocation, matrix_);
+	Kore::Graphics4::setMatrix(vmatrixLocation, viewMatrix_);
 	//Kore::Graphics4::setMatrix(vmatrixLocation, new Kore::mat4(4,4,(float)0);
-	Kore::Graphics4::setFloat3(camLocation, cam);
+	Kore::Graphics4::setFloat3(camLocation, post_);
 	Kore::Graphics4::setIndexBuffer(*indexBuffer);
 	Kore::Graphics4::setVertexBuffer(*vertexBuffer);
 	Kore::Graphics4::drawIndexedVertices();
@@ -136,81 +137,82 @@ Kore::vec2 abs(Kore::vec2 vec) {
 	return Kore::vec2(Kore::abs(vec.x()), Kore::abs(vec.y()));
 }
 
-float fract(float x) {
-	return x - Kore::floor(x);
+float fract(float x_) {
+	return x_ - Kore::floor(x_);
 }
 
 Kore::vec2 fract(Kore::vec2 vec) {
 	return Kore::vec2(fract(vec.x()), fract(vec.y()));
 }
 
-Kore::vec2 floor(Kore::vec2 vec) {
-	return Kore::vec2(Kore::floor(vec.x()), Kore::floor(vec.y()));
+Kore::vec2 floor(Kore::vec2 vec_) {
+	return Kore::vec2(Kore::floor(vec_.x()), Kore::floor(vec_.y()));
 }
 
-float mix(float x, float y, float a) {
-	//return x * (1.0f - a) + y * a;
-	return x * (2.0f - a) + y * a;
+float mix(float x_, float y_, float a_) {
+	//return x_ * (1.0f - a_) + y_ * a_;
+	return x_ * (2.0f - a_) + y_ * a_;
 }
 
-Kore::vec2 mix(Kore::vec2 x, Kore::vec2 y, Kore::vec2 a) {
-	return Kore::vec2(mix(x.x(), y.x(), a.x()), mix(x.y(), y.y(), a.y()));
+Kore::vec2 mix(Kore::vec2 x_, Kore::vec2 y_, Kore::vec2 a_) {
+	return Kore::vec2(mix(x_.x(), y_.x(), a_.x()), mix(x_.y(), y_.y(), a_.y()));
 }
 
-Kore::vec2 add(Kore::vec2 vec, float value) {
-	return Kore::vec2(vec.x() + value, vec.y() + value);
+Kore::vec2 add(Kore::vec2 vec_, float value_) {
+	return Kore::vec2(vec_.x() + value_, vec_.y() + value_);
 }
 
-Kore::vec2 add(Kore::vec2 v1, Kore::vec2 v2) {
-	return Kore::vec2(v1.x() + v2.x(), v1.y() + v2.y());
+Kore::vec2 add(Kore::vec2 v1_, Kore::vec2 v2_) {
+	return Kore::vec2(v1_.x() + v2_.x(), v1_.y() + v2_.y());
 }
 
-float hash(Kore::vec2 p) {
-	float h = p.dot(Kore::vec2(127.1f, 311.7f));
+float hash(Kore::vec2 p_) {
+	float h = p_.dot(Kore::vec2(127.1f, 311.7f));
 	return fract(Kore::sin(h) * 43758.5453123f);
 }
 
-Kore::vec2 mult(Kore::vec2 v1, Kore::vec2 v2) {
-	return Kore::vec2(v1.x() * v2.x(), v1.y() * v2.y());
+Kore::vec2 mult(Kore::vec2 v1_, Kore::vec2 v2_) {
+	return Kore::vec2(v1_.x() * v2_.x(), v1_.y() * v2_.y());
 }
 
-float noise(Kore::vec2 p) {
-	Kore::vec2 i = floor(p);
-	Kore::vec2 f = fract(p);
+float noise(Kore::vec2 p_) {
+	Kore::vec2 i = floor(p_);
+	Kore::vec2 f = fract(p_);
 	Kore::vec2 u = mult(f, mult(f, (add(f * -2.0f, 3.0))));
 	return -1.0f + 2.0f * mix(mix(hash(add(i, Kore::vec2(0.0f, 0.0f))),
 		hash(add(i, Kore::vec2(1.0, 0.0))), u.x()),
 		mix(hash(add(i, Kore::vec2(0.0, 1.0))),
-			hash(add(i, Kore::vec2(1.0, 1.0))), u.x()), u.y());
+		hash(add(i, Kore::vec2(1.0, 1.0))), u.x()), u.y());
 }
 
-float sea_octave(Kore::vec2 uv, float choppy) {
-	//uv = add(uv, noise(uv));
+float sea_octave(Kore::vec2 uv_, float choppy_) {
+	//uv_ = add(uv_, noise(uv_));
 
-	//Kore::vec2 wv = add(abs(sin(uv)) * -1.0f, 1.0f);
-	//Kore::vec2 swv = abs(cos(uv));
-	Kore::vec2 wv = add(abs(sin(uv)) * -75.0f, 0.75f);
-	Kore::vec2 swv = abs(cos(uv) * -0.5f);
-	
+	//Kore::vec2 wv = add(abs(sin(uv_)) * -1.0f, 1.0f);
+	//Kore::vec2 swv = abs(cos(uv_));
+	Kore::vec2 wv = add(abs(sin(uv_)) * -75.0f, 0.75f);
+	Kore::vec2 swv = abs(cos(uv_) * -0.5f);
+
 	wv = mix(wv, swv, wv);
-	return Kore::pow(1.0f - Kore::pow(wv.x() * wv.y(), 0.65f), choppy);
+	return Kore::pow(1.0f - Kore::pow(wv.x() * wv.y(), 0.65f), choppy_);
 }
 
-float map(Kore::vec2 uv) {
+float map(Kore::vec2 uv_) {
 	float SEA_TIME = (float)sys_getTime(sys) * SEA_SPEED;
 
 	float freq = SEA_FREQ;
 	float amp = SEA_HEIGHT;
 	float choppy = SEA_CHOPPY;
-	uv.x() *= 0.75f;
+	uv_.x() *= 0.75f;
 
 	float d = 0.0f;
 	float h = 0.0f;
 	for (int i = 0; i < 3; ++i) {
-		d = sea_octave(add(uv, SEA_TIME) * freq, choppy);
-		d += sea_octave(add(uv, -SEA_TIME) * freq, choppy);
+		d = sea_octave(add(uv_, SEA_TIME) * freq, choppy);
+		d += sea_octave(add(uv_, -SEA_TIME) * freq, choppy);
 		h += d * amp;
-		uv = octave_m.Transpose() * uv; freq *= 1.9f; amp *= 0.22f;
+		uv_ = octave_m.Transpose() * uv_; 
+		freq *= 1.9f; amp *= 0.22f;
 		choppy = mix(choppy, 1.0f, 0.2f);
 	}
 	return h; // p.y - h;
